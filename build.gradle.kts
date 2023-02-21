0298c4a93e37d3fad99a158a1ae8a8b83b9e1b09
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-02-21 10:42:28 +0100

    WIP

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    id("org.openrewrite.rewrite") version "5.36.0"
+}
+
+group = "io.arrow-kt"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+tasks.named<JavaCompile>("compileJava") {
+    options.release.set(8)
+}
+
+tasks.test {
+    useJUnitPlatform()
+
+}
+
+rewrite {
+    activeRecipe(
+        "arrow.SayHelloRecipe"
+    )
+}
+
+dependencies {
+    annotationProcessor("org.projectlombok:lombok:latest.release")
+
+    implementation(platform("org.openrewrite.recipe:rewrite-recipe-bom:1.15.0"))
+    compileOnly("org.openrewrite:rewrite-core")
+    compileOnly("org.projectlombok:lombok:latest.release")
+    implementation("org.openrewrite:rewrite-java")
+
+    implementation("org.openrewrite:rewrite-kotlin:0.2.0")
+
+
+    testImplementation("org.openrewrite:rewrite-test")
+    testImplementation("org.openrewrite.recipe:rewrite-testing-frameworks")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:latest.release")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:latest.release")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:latest.release")
+    testRuntimeOnly("ch.qos.logback:logback-classic:1.2.+")
+    testImplementation("org.assertj:assertj-core:latest.release")
+}
